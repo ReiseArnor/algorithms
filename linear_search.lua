8a9306a6b1ad37f9dd867524aa5566a4c728de14
Odanis Esquea <odanisdejesus@gmail.com>
2020-10-10 09:50:16 -0400

    Changed the for loops

--- linear_search.lua
+++ linear_search.lua
@@ -2,8 +2,8 @@ function busqueda(lista, objetivo)
 -- Search for a number in a random list of integers. Big-O Complexity: O(n)
   match = false
 
-  for elemento = 0, lista.size(), 1 do
-    if lista[elemento] == objetivo then
+  for k, elemento in ipairs(lista) do
+    if elemento == objetivo then
       match = true
       break
     end
@@ -21,12 +21,10 @@ function random_list(size)
     list[i] = math.random(0, 100)
   end
 
-  -- added a function to know the size of the array
-  list.size = function() return size end
   return list
 end
 
 list_int = random_list(20)
 ENCONTRADO = busqueda(list_int, 7)
-for elemento = 0, list_int.size(), 1 do print(list_int[elemento]) end
+for k, elemento in ipairs(list_int) do print(elemento) end
 if ENCONTRADO == true then print("esta") else print("no esta") end
